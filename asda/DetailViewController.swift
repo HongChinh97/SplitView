774e573a695bbb74c4340e46fcc2ca98c48280d0
admin <chiinhuv@gmail.com>
2018-08-16 10:37:06 +0700

    new

--- asda/DetailViewController.swift
+++ asda/DetailViewController.swift
@@ -1,45 +1,55 @@
 //
 //  DetailViewController.swift
-//  asda
+//  SpitView
 //
-//  Created by admin on 8/16/18.
+//  Created by admin on 8/11/18.
 //  Copyright © 2018 admin. All rights reserved.
 //
 
 import UIKit
 
-class DetailViewController: UIViewController {
-
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
+class DetailViewController: UIViewController, UIScrollViewDelegate {
+    
+    //    var dataCity: UIImage?
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     func configureView() {
         // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.description
+        if let detail = newName {
+            //            if let label = detailDescriptionLabel {
+            //                label.text = detail.description
+            if let photo = imageView {
+                photo.image = UIImage(named: detail)
             }
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        scrollView.delegate = self
+        //        scrollView.minimumZoomScale = 1.0
+        //        scrollView.maximumZoomScale = 10.0
         configureView()
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    var detailItem: NSDate? {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    var newName: String? {
         didSet {
             // Update the view.
             configureView()
         }
     }
-
-
+    
+    
 }
 
